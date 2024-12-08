8c6931ae6aec2011c835e2b94104ff6efb329c13
wusthy <wusthy@126.com>
2024-12-08 12:08:21 +0800

    houyao分支，first commit

---
+++ src/main/java/Test2.java
@@ -0,0 +1,9 @@
+/**
+@author wustkkh 
+@create 2024-12-08 11:46 
+*/public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是第一个版本");
+        System.out.println("这是houyao分支的第一个版本");
+    }
+}
